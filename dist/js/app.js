978c237a9e390024b1a628e168970b5009394bed
Leander Doerk <leander@doerk.org>
2024-02-09 23:12:24 +0100

    line function to draw a line from A to B and cmInPixel so the user can enter his gap size and it will get displayed correctly

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,12 @@
-alert("Henlo");
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+function line(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+function cmInPixel(cm) {
+    return cm * 37.7952756;
+}
+line(0, 10, cmInPixel(2), 10);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-alert("Henlo");
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
+function line(x1: number, y1: number, x2: number, y2: number) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+function cmInPixel(cm: number){
+    return cm * 37.7952756
+}
+
+line(0,10, cmInPixel(2),10);
