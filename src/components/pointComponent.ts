a194a7a7769d131042e7849464621abcd973baab
Leander Doerk <leander@doerk.org>
2024-03-16 16:49:45 +0100

    input html

--- src/components/pointComponent.ts
+++ src/components/pointComponent.ts
@@ -10,14 +10,31 @@ export default class PointElement extends HTMLElement{
 
     html() {
         this.shadowRoot.innerHTML += /*html*/`
-         <h1>Point</h1>   
+            <input type=number class="x" placeholder='X'></input>   
+            <input type=number class="y" placeholder='Y'></input>   
+            <input type=number class="z" placeholder='Z'></input>   
+            <input type=button class="delete" value="X"></input>
         `
     }
 
     css(){
-        this.shadowRoot.innerHTML +=  /*css*/`
+        this.shadowRoot.innerHTML +=  '<style>' +/*css*/`
+            input{
+                width: 40px;
+            }
+            .reset{
+                text-align: center;
+                vertical-align: middle;
+
+                background: red;
+                border: none;
+
+                padding: 3px;
+                border-radius: 4px
+
+            }
             
-        `
+        `+'</style>'
     }
 }
 
